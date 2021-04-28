4554f4bcedb4e36b3ab0477bc963cdc5ee71d3bd
charles delannoy <charles.delannoy@hei.fr>
2021-04-28 16:38:36 +0200

    let user to be empty for url

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation' do
+    before(:each) do
+      User.destroy_all
+    end
+
     it 'should check the uniqueness of the username' do
       create(:user)
       user = build(:user)
