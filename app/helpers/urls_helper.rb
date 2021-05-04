8f467e680d7c8099dd2b5343af3040c43d270f37
charles delannoy <charles.delannoy@hei.fr>
2021-05-04 10:40:46 +0200

    helper for url cards

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -3,4 +3,9 @@ module UrlsHelper
     url = "#{root_url}#{saved_url.generated_token}"
     render 'pages/saved_url', url: url
   end
+
+  def url_card_tag(url)
+    short_url = "#{root_url}#{url.generated_token}"
+    render 'urls/url_card', url: url, short_url: short_url
+  end
 end
